f43cd4e45626aa3039c6ac905880ea746f290833
Marcos Coelho <mparav124@gmail.com>
2022-06-13 17:23:11 -0300

    commit teste 1

---
+++ css/home/header-index.js
@@ -0,0 +1,21 @@
+document.write(`
+
+<header class="header-bg">
+<section class="header-container">
+  <a href="index.html" class="header-image">
+    <img src="imgs/logos-e-slides/logo-white.png" alt="logo doce tentação" width="123" height="90">
+  </a>
+  <nav class="header-content">
+    <button class="btn-menu">Menu
+      <span class="hamburger"></span>
+    </button>
+    <ul class="menu">
+      <li><a href="conheca-nos.html">Conheça-nos</a></li>
+      <li><a href="contato.html">Contacto</a></li>
+      <li><a href="ementa.html">Nossa Ementa</a></li>
+    </ul>
+  </nav>
+</section>
+</header>
+
+`);
